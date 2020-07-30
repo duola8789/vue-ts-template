be3a2d0e390ca8f4013a1d97ea76784c2f40d2c0
zhouhao15 <zhouhao15@baidu.com>
2020-07-30 16:06:08 +0800

    feat: 添加 px 转换 vw/vh 的方法

--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,14 @@
 const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // 将打包后的 JS/CSS/IMG/FONTS 等资源统一放到 static 目录中
 const ASSERTS_DIR = 'static';
 
 module.exports = {
-    lintOnSave: process.env.NODE_ENV === 'development' ? 'warning' : false,
-    assetsDir: process.env.NODE_ENV !== 'development' ? ASSERTS_DIR : '',
+    lintOnSave: isDev ? 'warning' : false,
+    assetsDir: isDev ? '' : ASSERTS_DIR,
     devServer: {
         port: 8006,
         disableHostCheck: true
@@ -52,7 +54,8 @@ function addStyleResource(rule) {
         .options({
             patterns: [
                 path.resolve(__dirname, './src/styles/variables.scss'),
-                path.resolve(__dirname, './src/styles/mixins.scss')
+                path.resolve(__dirname, './src/styles/mixins.scss'),
+                path.resolve(__dirname, './src/styles/functions.scss'),
             ]
         });
 }
